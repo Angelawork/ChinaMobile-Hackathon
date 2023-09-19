f1fca820f32fec2eaa8c7b5b7dabda8401749eb6
Angel <2162556631@qq.com>
2023-09-18 21:50:22 -0400

    Model Version 2

--- output.py
+++ output.py
@@ -3,13 +3,11 @@ class Output:
     def __init__(self, path):
         self.path = path
         self.output=""
-        self.file = open(path, 'w', encoding='utf-8')
-        self.board=""
 
     def append(self, content: list):
-        self.board+=f"{content[0]},{content[1].strftime('%Y-%m-%d %H:%M')},{content[2]}\n"
+        self.output+=f"{content[0]},{content[1].strftime('%Y-%m-%d %H:%M')},{content[2]}\n"
 
-    def close(self):
+    def save(self):
         self.file = open(self.path, 'w', encoding='utf-8')
         self.file.write("异常账号,异常时间,异常类型\n")
         self.file.write(f"{self.output}\n")
