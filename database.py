e514eb943142c9b20e1aab39cac01a50380f5111
Angel <2162556631@qq.com>
2023-09-22 15:52:53 -0400

    Annotations draft1

--- database.py
+++ database.py
@@ -1,28 +1,29 @@
 class Database:
-  def __init__(self,account):
+  def __init__(self, account):
     self.account = account
-    self.operations ={"time":[],"action":[],"ip":[]}
+    self.operations = {"time": [], "action": [], "ip": []}
     # 记录每个新分钟的第一个元素的index
     self.time_index = [0]
 
   def sort_time(self):
-    self.operations=sorted(zip(self.operations['time'], self.operations['action'], self.operations['ip']))
+    # 对操作记录按时间排序
+    self.operations = sorted(zip(self.operations['time'], self.operations['action'], self.operations['ip']))
 
-    curr=self.operations[0][0]
+    curr = self.operations[0][0]
     for o in range(len(self.operations)):
-      if self.operations[o][0]!=curr:
+      if self.operations[o][0] != curr:
         self.time_index.append(o)
         curr = self.operations[o][0]
 
   def add_entry(self, time, action, ip):
+    # 添加新的操作记录
     self.operations["time"].append(time)
     self.operations["action"].append(action)
     self.operations["ip"].append(ip)
 
-#for testing----
+  # 用于测试,显示database object中含有的内容
   def print_entries(self):
     for entry in self.operations:
       time, action, ip = entry
       print(f"Time: {time}, Action: {action}, IP: {ip}")
     print(f"time_index: {self.time_index}")
-#----
--- preprocess.py
+++ preprocess.py
@@ -4,33 +4,39 @@ from database import *
 from algorithm import singly_linked_list
 
 from collections import defaultdict
+
+# 创建一个 defaultdict 用于按照类别分组数据
 grouped_data = defaultdict(list)
 
 def main(file_path):
-    with open(file_path,'r', encoding='utf-8') as file:
-        csv_reader=csv.reader(file)
-        #skip 1st row
+    with open(file_path, 'r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        # 跳过第一行标题行
         next(csv_reader)
 
         for row in csv_reader:
-            category=row[2]
+            category = row[2]
             grouped_data[category].append(row)
 
+    # 创建一个单链表 data_list
     data_list = singly_linked_list.LinkedList()
+
+    # 遍历分组数据并创建数据库对象
     for category, group in grouped_data.items():
-        curr_database = Database(group[0][2])
+        curr_database = Database(group[0][2])  # 使用组内第一个元素创建数据库对象
         for row in group:
             curr_action = tools.clean_access(row[4])
             curr_date = tools.convert_time(row[0])
             curr_ip = row[5]
 
             curr_database.add_entry(curr_date, curr_action, curr_ip)
-        curr_database.sort_time()
-        data_list.insert_tail(curr_database)
 
-    #testing to print----
+        curr_database.sort_time()  # 对数据库对象排序
+        data_list.insert_tail(curr_database)  # 将数据库对象添加到单链表尾部
+
+    # 用于测试的代码，打印数据
     '''for node in data_list:
         node.print_entries()'''
-    #----
+    # 返回单链表
     return data_list
 
